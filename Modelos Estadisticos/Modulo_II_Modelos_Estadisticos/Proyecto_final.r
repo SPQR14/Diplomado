139fd84eacc3f8b83d49ee7f45628176734cc568
SPQR14 <albertopicolara@gmail.com>
2020-05-31 14:12:18 -0500

    Descanso de sólo leer los datos

--- Modelos Estadisticos/Modulo_II_Modelos_Estadisticos/Proyecto_final.r
+++ Modelos Estadisticos/Modulo_II_Modelos_Estadisticos/Proyecto_final.r
@@ -3,16 +3,14 @@
 ### Fecha fin:                5/06/2020 ###
 
 #Usando la ruta absoluta del equipo en el que se programó este Script
-#Nombre del archivo del conjunto de datos: Consumo_Gasolina_Autos_Ene_2018.csv
-install.packages("readxl")
-library("readxl")
+#Nombre del archivo del conjunto de datos: Consumo_Gasolina_Autos_Ene_2018.xlsx
+
+library(readxl)
 library(utf8)
 
 setwd("/home/spqr14/Desarrollo/Diplomado/Modelos Estadisticos/Modulo_II_Modelos_Estadisticos/")
 
-df = read_excel("/home/spqr14/Desarrollo/Diplomado/Modelos Estadisticos/Datos/Consumo_Gasolina_Autos_Ene_2018.xlsx")
-
-
+df = read_excel("../Datos/Consumo_Gasolina_Autos_Ene_2018.xlsx")
 
 dim(df)
 names(df)
@@ -20,5 +18,7 @@ str(df)
 
 attach(df)
 
+tabla_marca <- table(Marca);tabla_marca
+tabla_submarca <- table(Submarca);tabla_submarca
 
-
+barplot(tabla_marca)
