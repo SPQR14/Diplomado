7098155611a8d160b6f62e9ddb149d74dce4c5ff
SPQR14_ <albertopicolara@gmail.com>
2020-06-20 12:54:18 -0500

    Datos Limpios buenos y graficas

--- Modelos Estadisticos/Modulo_II_Modelos_Estadisticos/Ejercicio_Weibull.r
+++ Modelos Estadisticos/Modulo_II_Modelos_Estadisticos/Ejercicio_Weibull.r
@@ -16,27 +16,64 @@
 
 # Entregar un reporte en donde escribas el codigo y presentes los resultados
 # graficos.
+
+library(MASS)
+
 lluvias= read.table("D:/Desarrollo/Diplomado/Modelos Estadisticos/Datos/lluvias.dat",sep="\t",row.names = 1,header=T)
 attach(lluvias)
-y= Precipitacion
-
+y = Precipitacion
 
 hist(y,breaks=seq(0,2400,300),main= "Precipitacion anual en Mexico (2017)",
-     xlab="Precipitacion (mm)")
+     xlab="Precipitacion (mm)", col = heat.colors(14))
 
-plot(seq(0,30,0.1),dweibull(seq(0,30,0.1),shape=2,scale=2),ty="l",xlab="x",ylab="f(x)")
+plot(seq(0,30,0.01),dweibull(seq(0,30,0.01),shape=2,scale=2),ty="l",xlab="x",ylab="f(x)")
+title("Curva de la función de distribución Weibull")
 
+# a) Escribe la funcion de (log)verosimilitud para los datos de lluvias 
+# usando el modelo Weibull.
+
+log_ver_wei <- function(theta) { 
+  l= sum(dweibull(y,theta[1],theta[2],log=TRUE))
+  return(-l)
+}
+
+# b) Estima los parametros del modelo usando alguno de los optimizadores
+# discutidos en clase. Como valores iniciales considera las estimaciones
+# de momentos (ver notas de clase).
 equis_barra = mean(y);equis_barra
 ese = var(y);ese
-
 factor1 = ese/equis_barra^2;factor1
 
-plot(seq(0,5,0.5),  factor1 - ( gamma(1+(2/seq(0,5, 0.5)))/gamma(1+(1/seq(0,5,0.5)))^2 ) + 1, ty = "l")
-locator()#con locator se halla un alfa aprox. 1.934, 1.935
-abline(h=0,v = 1.935, col="red",lty=3)
 
-alfa_gorro = 1.935
-beta_gorro = equis_barra/gamma(1+(1/alfa_gorro));beta_gorro
+plot(seq(0,5,0.5),  factor1 - ( gamma(1+(2/seq(0,5, 0.5)))/gamma(1+(1/seq(0,5,0.5)))^2 ) + 1, ty = "l", main = "Aproximación por
+     método gráfco del parámetro alfa = 1.975")
+#locator() #con locator() se halla un alfa aprox. 1.975
+abline(h=0,v = 1.975, col="red",lty=3)
 
+alfa_goro_momentos = 1.975;alfa_gorro_momentos
+beta_gorro_momentos = equis_barra/gamma(1+(1/alfa_gorro_momentos));beta_gorro_momentos
+
+ajuste_2<- optim(c(alfa_goro_momentos,beta_gorro_momentos),log_ver_wei);ajuste_2
+alfa_log_ver = ajuste_2$par[1]; alfa_log_ver
+beta_log_ver = ajuste_2$par[2]; beta_log_ver
+# c) Grafica el histograma de los datos de lluvia y ajusta el modelo Weibull
+# con los parametros estimados en b).
+hist(y, pch=20, prob=TRUE,main="")
+curve(dweibull(x, alfa_log_ver,beta_log_ver),col="blue", lwd=2, add=T)
+
+# d) Compara tus ajustes con los obtenidos mediante la funcion "fitdistr".
+ajuste_3 = fitdistr(y,densfun = "weibull");ajuste_3
+str(ajuste_3)
+
+# e) Utiliza el criterio AIC o BIC para seleccionar entre el modelo "gamma"
+# y el modelo "Weibull". ¿Cual es el modelo preferido para explicar el
+# comportamiento de las lluvias en Mexico?
+k=2   # numero de parametros a estimar
+
+AIC_gamma= 482.3427#2*k-(2*lluvias_mle1$loglik); AIC_gamma
+BIC_gamma= 485.2742#log(n)*k-(2*lluvias_mle1$loglik); BIC_gamma
+n = length(y)
+AIC_weibull = 2*k-(2*ajuste_3$loglik);AIC_weibull
+BIC_weibull = log(n)*k-(2*ajuste_3$loglik);BIC_weibull
 
 
--- Modelos Estadisticos/Modulo_II_Modelos_Estadisticos/Proyecto_final.r
+++ Modelos Estadisticos/Modulo_II_Modelos_Estadisticos/Proyecto_final.r
@@ -7,6 +7,7 @@
 
 library(readxl)
 library(utf8)
+library(ggplot2)
 
 setwd("../Desarrollo/Diplomado/Modelos Estadisticos/Modulo_II_Modelos_Estadisticos/")
 
@@ -19,7 +20,8 @@ str(df)
 
 attach(df)
 
-tabla_transmision_fa = table(Trans_resumen);tabla_transmision
+#Tablas y gráficos por transmisión
+tabla_transmision_fa = table(Trans_resumen);tabla_transmision_fa
 tabla_transmision_fr = prop.table(tabla_transmision_fa);tabla_transmision_fr
 
 barplot(tabla_transmision_fa, col=3:4, ylim=c(0,3500))
@@ -27,4 +29,29 @@ title("Diagrama de barras - Transmisión \n (Frecuencias absolutas)")
 
 barplot(tabla_transmision_fr, col=3:4, ylim=c(0,0.8))
 title("Diagrama de barras - Transmisión \n (Frecuencias relativas)")
+
+
+#Tablas y graficos por combustible
+tabla_combustible_fa = table(df$Comb.);tabla_combustible_fa
+table_combustible_fr = prop.table(tabla_combustible_fa); table_combustible_fr
+
+barplot(tabla_combustible_fa, col=3:4, ylim=c(0,5500))
+title("Diagrama de barras - Combustible \n (Frecuencias absolutas)")
+
+barplot(table_combustible_fr, col=3:4, ylim=c(0,1))
+title("Diagrama de barras - Combustible \n (Frecuencias relativas)")
+
+#tablas y gráficos por marca
+tabla_marcas_fa = table(df$Marca);tabla_marcas_fa
+tabla_marcas_fr = prop.table(tabla_marcas_fa);tabla_marcas_fr
+par(las=2)
+barplot(tabla_marcas_fr, col=heat.colors(43), xlim=c(0,0.1), horiz = TRUE)
+title("Diagrama de barras - Combustible \n (Frecuencias relativas)")
+
+#tablas y gráficos por marca
+tabla_cilindros_fa = table(df$Cilindros);tabla_cilindros_fa
+tabla_cilindros_fr = prop.table(tabla_cilindros_fa);tabla_cilindros_fr
+par(las=1)
+barplot(tabla_cilindros_fr, col=heat.colors(43), ylim=c(0,0.6))
+title("Diagrama de barras - Número de cilindros \n (Frecuencias relativas)")
 
