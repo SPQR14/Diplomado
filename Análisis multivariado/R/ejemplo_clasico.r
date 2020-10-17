815d38aa664ae2e7b76b13af04deeefc8fe7223f
SPQR14 <albertopicolara@gmail.com>
2020-10-17 10:56:28 -0500

    P3 Análisis de factores

---
+++ Análisis multivariado/R/ejemplo_clasico.r
@@ -0,0 +1,12 @@
+ability.cov
+
+habilidad<-ability.cov$cov
+habilidad
+
+fa(habilidad,nfactors=1,fm="pa",rotate="promax")
+
+fa(habilidad,nfactors=2,fm="pa",rotate="promax")
+
+
+fa(habilidad,nfactors=3,fm="pa",rotate="promax")
+
